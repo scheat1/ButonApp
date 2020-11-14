5b9439cc5cda15b7fd3719a70af19a8e15a9930d
scheat.k@gmail.com <scheat.k@gmail.com>
2020-11-14 20:45:18 +0300

    Proje dosyası ekle.

---
+++ ButonApp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ButonApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.time = new System.Windows.Forms.Timer(this.components);
+            this.game = new System.Windows.Forms.Timer(this.components);
+            this.pnlPuan = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlPuan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.Interval = 1000;
+            this.time.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // game
+            // 
+            this.game.Interval = 1000;
+            this.game.Tick += new System.EventHandler(this.game_Tick);
+            // 
+            // pnlPuan
+            // 
+            this.pnlPuan.Controls.Add(this.label2);
+            this.pnlPuan.Controls.Add(this.label1);
+            this.pnlPuan.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlPuan.Location = new System.Drawing.Point(939, 0);
+            this.pnlPuan.Name = "pnlPuan";
+            this.pnlPuan.Size = new System.Drawing.Size(120, 554);
+            this.pnlPuan.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 554);
+            this.Controls.Add(this.pnlPuan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlPuan.ResumeLayout(false);
+            this.pnlPuan.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer time;
+        private System.Windows.Forms.Timer game;
+        private System.Windows.Forms.Panel pnlPuan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ButonApp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButonApp
+{
+    public partial class Form1 : Form
+    {
+        static int sonuc, sure = 30;
+        static string path = "C:/ButonOyunuPuanlar.txt";
+
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Puan";
+            label2.Text = "Süre";
+
+
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Dispose();
+
+            sonuc += int.Parse(b.Text);
+            label1.Text = $" Puan = {sonuc}";
+        }
+
+        private void time_Tick(object sender, EventArgs e)
+        {
+            if (sure > 0)
+            {
+                sure--;
+                label2.Text = "Süre: " + sure.ToString();
+            }
+            else
+            {
+                time.Stop();
+                game.Stop();
+                dynamic result = MessageBox.Show("Süre Bitti \nPuanınız: " + label1.Text + "\nYeniden Oynama İster Misiniz ?",
+                "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    FileWriter();
+                    Application.Restart();
+                }
+
+                if (result == DialogResult.No)
+                {
+                    FileWriter();
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void game_Tick(object sender, EventArgs e)
+        {
+            if (sure > 0)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(70, 35);
+                btn.Text = rnd.Next(100).ToString();
+                btn.Location = new Point(rnd.Next(this.ClientSize.Width - pnlPuan.Width - btn.Width), rnd.Next(this.ClientSize.Height - btn.Height));
+                btn.Visible = true;
+                this.Controls.Add(btn);
+                btn.Click += btnClick;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game.Start();
+            time.Start();
+        }
+
+        private void FileWriter()
+        {
+            FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(DateTime.Now + " " + "Puan: " + sonuc);
+            fs.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+    }
+}
+
